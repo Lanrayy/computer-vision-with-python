beccdd38c86b9bfed9212dd7b4e06afa4182fda2
Olanrewaju Sodeinde <lanre.sodeinde1@gmail.com>
2021-06-30 00:19:04 +0100

    Frame rate is now displayed on the screen

--- hand-tracking/hand-tracking-minumum.py
+++ hand-tracking/hand-tracking-minumum.py
@@ -15,6 +15,10 @@ hands = mpHands.Hands(False, 2, 0.5, 0.5)
 # the provided method that allows us to draw the points on the hand
 mpDraw = mp.solutions.drawing_utils
 
+# Print out the frame rate
+pTime = 0
+cTime = 0
+
 # Running a webcam
 while True:
     success, img = cap.read()
@@ -24,17 +28,21 @@ while True:
 
     # check if we have something in the results
     # We need to know if something is detected or not
-    print(results.multi_hand_landmarks)
-
+    # print(results.multi_hand_landmarks)
+    # check if we have multiple hands
     if results.multi_hand_landmarks:
         # For each hand extract the information of each hand
         for handLandMarks in results.multi_hand_landmarks:
             # Draw the points on the hand
-            mpDraw.draw_landmarks(img, handLandMarks)
+            # the HAND_CONNECTIONS input will draw the lines between the points of the hands
+            mpDraw.draw_landmarks(img, handLandMarks, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
 
-
-    # check if we have multiple hands
+    # Print the frame rate on screen
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
